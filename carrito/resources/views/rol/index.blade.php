e22568acc7b74efa5e64fa46a856b3f62c209d3c
Rodrigo Mauricio Torres Artiaga <rodmayi_@hotmail.com>
2017-11-04 20:54:17 -0600

    Enlaces editar y otras cosas :'V

--- carrito/resources/views/rol/index.blade.php
+++ carrito/resources/views/rol/index.blade.php
@@ -9,22 +9,24 @@
 	<table class="table">
 		<thead class="thead-dark">
 			<tr>
-				<th scope="col">id</th>
-				<th scope="col">nombre rol</th>
+				<th scope="col">Id</th>
+				<th scope="col">Nombre rol</th>
+        <th scope="col">Acción</th>
 			</tr>
-			
+
 		</thead>
 		<tbody>
 			@forelse($roles as $rol)
 			<tr>
 				<td>{{$rol->id}}</td>
 				<td>{{$rol->nombre_rol}}</td>
+          <td><a href="rol/{{$rol->id}}/edit">Editar</a></td>
 			</tr>
 			@empty
 				<h2>Aún no ha creado articulos.</h2>
-			@endforelse	
+			@endforelse
 		</tbody>
-		
+
 	</table>
 	<div class="offset-lg-6 offset-md-6 offset-sm-3 offset-xs-3">
 		{{$roles->links()}}
